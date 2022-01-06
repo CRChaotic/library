54aab647a5102c966818064657e8541f1ff9e3e7
CRC <90101801+CRChaotic@users.noreply.github.com>
2022-01-06 21:30:49 +0800

    Update Controller.java

--- MVC/Controller.java
+++ MVC/Controller.java
@@ -10,9 +10,6 @@ public abstract class Controller {
     private Model model;
     private View view;
 
-
-
-
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
